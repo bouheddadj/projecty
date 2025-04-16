077c6f00383e968a065019ded49d18b7be821af3
THIEBAUD Enzo <enzo.thiebaud@etu.univ-lyon1.fr>
2025-04-16 18:05:18 +0200

    feat: 5.2.4 done

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyLogin from '../components/Login.vue'
+</script>
+
+<template>
+  <main>
+    <MyLogin />
+  </main>
+</template>
+
---
+++ client/src/views/MyMapView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyMap from '../components/MyMap.vue'
+</script>
+
+<template>
+  <main>
+    <MyMap />
+  </main>
+</template>
+
