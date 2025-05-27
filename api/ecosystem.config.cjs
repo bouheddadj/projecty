aed6bc5de301dabbd59e6293ee1ae1f1d98a8450
enzo <enzo.thiebaud@etu.univ-lyon.fr>
2025-05-27 13:52:40 +0200

    Fix ENV

--- api/ecosystem.config.cjs
+++ api/ecosystem.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
       script: "./server.js",
       env_production: {
         NODE_ENV: "production",
+        PORT: 3000,
+        BASE_URL_USERS: "https://192.168.75.33:8443/users",
       },
     },
   ],
